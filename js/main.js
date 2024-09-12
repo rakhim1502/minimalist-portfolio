8034713328ada5d6eed6fb4c9d6129b33ddb57f4
rakhim1502 <uzbrm95@gmail.com>
2024-09-12 22:30:01 +0500

    Add about me section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const elsitenavItem = document.querySelectorAll('.sitenav__item');
-const elsitenavLink = elsitenavItem.querySelectorAll('.sitenav__link');
-if (elsitenavLink) {
-  elsitenavLink.addEventListener('click', function () {
-    elsitenavItem.classList.remove('sitenav__item--active');
-  });
+const modifiers = {
+  elSiteHeaderOpen: 'site-header--open'
+}
 
+const elSiteHeader = document.querySelector('.site-header');
+const elSiteHeaderToggler = elSiteHeader.querySelector('.site-header__toggler');
+
+if (elSiteHeaderToggler) {
+  elSiteHeaderToggler.addEventListener('click', function () {
+    elSiteHeader.classList.toggle(modifiers.elSiteHeaderOpen);
+  });
 }
